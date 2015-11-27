b63ebbe3b35bd1f94ba07cb807e9dddbbf8f90c1
RCBG <ralphgalido@gmail.com>
2015-11-27 11:56:51 +0800

    improved homepage UI

--- application/views/home.php
+++ application/views/home.php
@@ -3,36 +3,41 @@
         <image src="<?= base_url('assets/image/map.png') ?>" style="border: 3px solid #999999;"/>
     </div>	
     <div class="col-sm-5" style="margin:10px;">
-        <div class="row">
-            <p class="hotup col-sm-6">Mandaue City Hotlines</p>
-            <p class="hotup col-sm-4">System Updates</p>
-        </div>
+
+
         <div class="row">
             <div class="col-sm-6">
-                <div class="corner">
-                    <p>ERUF</p>
-                    <p>166</p>
-                    <p>Fire Department</p>
-                    <p>160</p>
-                    <p>SWAT</p>
-                    <p>166</p>
-                    <p></p>
-                    <p>REACT</p>
-                    <p>515-4455</p>
-                    <p>VECO (Power Services)</p>
-                    <p>230-8326</p>
-                    <p>MCWD (Water Services)</p>
-                    <p>412-1836</p>
+                <div class="panel panel-default">
+                    <!-- Default panel contents -->
+                    <div class="panel-heading">Emergency Numbers</div>
+
+                    <!-- List group -->
+                    <ul class="list-group">
+                        <li class="list-group-item">ERUF 166</li>
+                        <li class="list-group-item">Fire Department 160</li>
+                        <li class="list-group-item">SWAT 166</li>
+                        <li class="list-group-item">REACT 515-4455</li>
+                        <li class="list-group-item">VECO (Power Sevices) 230-8326</li>
+                        <li class="list-group-item">MCWD (Water Sevices) 412-1836</li>
+                    </ul>
                 </div>
             </div>
-            <div class="col-sm-3">
-                <div class="corner">
-                    October 3, 2015<br>
-                    There is a Maintenance on October 23, 2015. <br><br>
-                    September 27, 2015<br>
-                    System was Updated.<br><br>
-                    September 20, 2015<br>
-                    Some of design on website were changed.
+            <div class="col-sm-6">
+                <div class="panel panel-default">
+                    <!-- Default panel contents -->
+                    <div class="panel-heading">Announcements</div>
+                    <div class="panel-body">
+                        <p>October 3, 2015</p>
+                        <p>There is a Maintenance on October 23, 2015.</p>
+                    </div>
+                    <div class="panel-body">
+                        <p>September 27, 2015</p>
+                        <p>System was Updated.</p>
+                    </div>
+                    <div class="panel-body">
+                        <p>September 20, 2015</p>
+                        <p>Some of design on website were changed.</p>
+                    </div>
                 </div>
             </div>
         </div>
